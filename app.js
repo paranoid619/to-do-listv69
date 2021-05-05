146c4ea334d497efb061c043d1286ecb5fcdcd7d
paranoid619 <souravbot123@gmail.com>
2021-05-05 18:43:51 +0530

    initial commit

--- app.js
+++ app.js
@@ -163,10 +163,10 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
- let port = process.env.PORT;
- if (port == null || port == ""){
-   port = 3000;
- }
+ 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.listen(port, function() {
   console.log("Server started successfully");
